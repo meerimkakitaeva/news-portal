5a56fc7e8c60cad7e2dae02e12618b41c05544f1
Meerim K <thvrim@gmail.com>
2023-08-26 13:42:17 +0600

    добавила базовый url, файловый инпут, глобальный store , хуки и добавила в index.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {CssBaseline} from "@mui/material";
 
 const App = () => {
   return (
-      <div>
-        
-      </div>
+      <>
+        <CssBaseline />
+      </>
   );
 };
 
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import { ThemeProvider } from '@mui/material';
 import App from './App';
+import {store} from "./app/store";
+import theme from "./theme";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <React.StrictMode>
+                    <App />
+                </React.StrictMode>
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>
 );
-
